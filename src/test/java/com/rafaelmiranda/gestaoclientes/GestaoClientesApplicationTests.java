d94eb42b65b8be60b568886b176ac8c660d85f39
Rafael Piva <rafaelpiva@outlook.com>
2025-08-13 10:50:41 -0300

    criado spring e conexão com postgreSQL

---
+++ src/test/java/com/rafaelmiranda/gestaoclientes/GestaoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmiranda.gestaoclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
